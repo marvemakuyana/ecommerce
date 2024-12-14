91ea5e6c02c1f12a2c115dc0adccdd611d588589
marvemakuyana <marvellousmakuyana@gmail.com>
2024-12-14 12:15:23 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
